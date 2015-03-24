e41ae16217a4b83ad2b73fef67b5f5dd0fe6f7bb
Pim Verlangen <pim.verlangen@gmail.com>
2015-03-24 11:33:39 +0100

    started geolocation

---
+++ www/js/geolocation.js
@@ -0,0 +1,29 @@
+// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+
+
+var onSuccess = function(position) {
+    window.localStorage.setItem("lat", position.coords.latitude);
+    window.localStorage.setItem("lon", position.coords.longitude);
+
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+var onError = function(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+
+//navigator.geolocation.getCurrentPosition(onSuccess, onError);
--- www/js/main.js
+++ www/js/main.js
@@ -1,51 +1,161 @@
+var restaurants = [];
+
+var currentItems = 1;
+var initialItems = 10;
+var perPage = 10;
+var currentPage = 1;
+var filter = "all";
+
 $(document).ready(function () {
-	$('#item-list').bind('pagebeforeshow', function() {
-		$('.content-list').listview('refresh');
+	$('#item-description').dotdotdot({height: 150, watch: "window"});
+	
+	$(document).on("scrollstop", checkScroll);
+	
+	$('#close-by-restaurants-button').on('click', function(e) {
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		$('.content-list').empty();
+		filter = "close-by";
 	});
 
-	$('.type-container > a').on('click', function() {
-		//Fill item-list
-		loadListContent(this);
-		
-	});		
+	$('#all-restaurants-button').on('click', function(e) {
+		$('.content-list').empty();
+		filter = "all";
+	});
+
+	$('#item-list').bind('pagebeforeshow', function() {
+		currentPage = 1;
+		perPage = 10;
+		if (filter === "close-by") {
+			loadClosebyRestaurants(currentPage++, perPage);
+		} else if (filter === "all") {
+			loadRestaurants(currentPage++, perPage);
+		}
+	});
+
+	$('#item-list').bind('pageshow', function() {				
+		$('.content-list').listview('refresh');
+	});
 });
 
-function loadListContent(anchor) {
-	var type = $(anchor).attr('data-type');
+function loadRestaurants(pageNr, perPage) {
+	if (!perPage) {
+		perPage = 10;
+	}
 	
-	var data = retrieveDataByType(type);
+	if (!pageNr) {
+		pageNr = 1;
+	}
+	
+	$.ajax({
+		type: "GET",
+		data: {
+			page: pageNr,
+			per_page: perPage
+		},
+		url:       'https://api.eet.nu/venues/',
+		dataType:  'json',
+		success:   function (response, textStatus, jqXHR) {
+			//Pagination
+			var currentPage = response.pagination.current_page;
+			var totalPages = response.pagination.total_pages;
+			
+			//Restaurants
+			var results = response.results;
+			restaurants = $.merge(restaurants, results);
+			
+			var items = "";
+			$.each(results, function(index, element) {
+				if (element.images.original.length < 1) {
+					element.images.original.push("img/no-img.jpg");
+				}
+				items += '<li><a id="' + element.id + '"  href="#detail-page"><img class="ui-li-thumb" src="' + element.images.original[0] + '">' + element.name + '</a></li>';
+			});
+			results.items = items;
+			$(".content-list").append(items).listview('refresh');
+			attachListeners();
+		}
+	});
+}
 
-	//Clear list
-	$('.content-list').empty();
+function loadClosebyRestaurants(pageNr, perPage) {
+	if (!perPage) {
+		perPage = 10;
+	}
 	
-	//Append data
-	$.each(data, function(index, element) {
-		var listItem = '<li><a id="' + element.id + '"  data-type="' + element.type + '" href="#detail-page"><img class="ui-li-thumb" src="img/' + element.img + '">' + element.name + '</a></li>';
-		$('.content-list').append(listItem);
-	});	
+	if (!pageNr) {
+		pageNr = 1;
+	}
+	https://api.eet.nu/venues?geolocation=51.55,5.7&sort_by=distance
+	$.ajax({
+		type: "GET",
+		data: {
+			page: pageNr,
+			per_page: perPage,
+			geolocation: window.localStorage.get("lat") + "," + window.localStorage.get("lon"),
+			sort_by: "distance"
+		},
+		url:       'https://api.eet.nu/venues/',
+		dataType:  'json',
+		success:   function (response, textStatus, jqXHR) {
+			//Pagination
+			var currentPage = response.pagination.current_page;
+			var totalPages = response.pagination.total_pages;
+			
+			//Restaurants
+			var results = response.results;
+			restaurants = $.merge(restaurants, results);
+			
+			var items = "";
+			$.each(results, function(index, element) {
+				if (element.images.original.length < 1) {
+					element.images.original.push("img/no-img.jpg");
+				}
+				items += '<li><a id="' + element.id + '"  href="#detail-page"><img class="ui-li-thumb" src="' + element.images.original[0] + '">' + element.name + '</a></li>';
+			});
+			results.items = items;
+			$(".content-list").append(items).listview('refresh');
+			attachListeners();
+		}
+	});
+}
+
+function loadSingleRestaurant(id) {
+	//TO-DO: Validate
+	$.ajax({
+		type: "GET",
+		url:       'https://api.eet.nu/venues/' + id,
+		dataType:  'json',
+		success:   function (response, textStatus, jqXHR) {
+				if (response.images.original.length < 1) {
+					response.images.original.push("img/no-img.jpg");
+				}
+			loadDetailPage(response);
+		}
+	});
 	
-	attachListeners();	
 }
 
 function attachListeners() {
+	//Remove any click-listeners.sdf
+	$('.content-list > li > a').off('click');
+	
+	//Attach new click-listeners
 	$('.content-list > li > a').on('click', function() {
-		var id = parseInt($(this).attr('id'));
+		//$(this).off('click');
+		var id = $(this).attr('id');
 		var type = $(this).attr('data-type');
-		var itemDescription = $('#item-description');
-		var data = retrieveDataByType(type);
-		var object = $.grep(data, function(e){ return e.id == id; });
-		if (object) {
-			loadDetailPage(object[0]);
-		}
+		loadSingleRestaurant(id);
 	});
 }
 
 function loadDetailPage(item) {
 	$('#item-title').text(item.name);
+
 	$('#item-description').text(item.description);
-	
+	$('#item-phonenumber').empty();
+	$('#item-phonenumber').append('<a href="tel:' + item.telephone + '">' + item.telephone + '</a>');
 	$('#item-img-container').empty();
-	$('#item-img-container').append('<img style="border-style: groove;" src="img/' + item.img + '" height="150" height="150">');
+	$('#item-img-container').append('<img style="border-style: groove;" src="' + item.images.original[0] + '" height="150" height="150">');
 }
 
 function checkScroll() {
@@ -76,7 +186,8 @@ function checkScroll() {
        load more elements  */
     if (activePage[0].id == "item-list" && scrolled >= scrollEnd) {
         /* run loadMore function */
-        addMore(activePage);
+        addMore();
+		//loadRestaurants(currentPage++);
     }
 }
 
@@ -89,37 +200,17 @@ function addMore(page) {
 	});
 	
 	setTimeout(function() {
-		var items = '';
-		var last = $("li" , page).length;
-		var cont = last + 5;
-		
-		for (var i = last; i < cont; i++) {
-			items += '<li><a id="' + (9000+i) + '"  data-type="unknown" href="#detail-page"><img class="ui-li-thumb" src="img/no-img.jpg">Element' + i + ' after scroll.</a></li>';
+		//loadRestaurants(currentPage++)
+		if (filter === "close-by") {
+			loadClosebyRestaurants(currentPage++, perPage);
+		} else if (filter === "all") {
+			loadRestaurants(currentPage++, perPage);
 		}
-		$(".content-list").append(items).listview('refresh');
+		
+		attachListeners();	
 		$.mobile.loading("hide");
 		$(document).on("scrollstop", checkScroll);
 	}, 500);
 }
 
-function retrieveDataByType(type) {
-	var data;
-	switch(type) {
-		case 'movie':
-			data = moviesData;
-			break;
-		case 'tv-show':
-			data = showsData;
-			break;
-		case 'cartoon':
-			data = cartoonsData;
-			break;
-		default:
-			data = [];
-			break;
-	}
-	return data;
-}
-
-$(document).on("scrollstop", checkScroll);
 
